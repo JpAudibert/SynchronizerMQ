3f2e73f8240cd728c101e5107ab6c12e2d5a9a84
João Pedro Basso Audibert <hotsjoao@gmail.com>
2021-12-15 22:19:58 -0300

    Added external files for reading

---
+++ Synchronizer/Synchronizer/Helper/FileReader.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Synchronizer.Helper;
+
+public class FileReader
+{
+    public static List<string> ReadFile(string file)
+    {
+        string[] lines = Array.Empty<string>();
+
+        if (File.Exists(file))
+        {
+            lines = File.ReadAllLines(file);
+        }
+
+        List<string> linesList = lines.ToList();
+
+        return linesList;
+    }
+
+    public class PathCombiner
+    {
+        public static string combineInputPaths(List<string> fileToCombine)
+        {
+            string currentDir = Environment.CurrentDirectory;
+            fileToCombine.Insert(0, currentDir);
+            string fileCombined = Path.GetFullPath(Path.Combine(fileToCombine.ToArray()));
+            return fileCombined;
+        }
+    }
+}
